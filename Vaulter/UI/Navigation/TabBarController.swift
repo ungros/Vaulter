b54ee37ff1276410499190ba13d3cecc11e247b9
ungros <82379236+ungros@users.noreply.github.com>
2023-01-07 21:22:31 +0300

    Structure

---
+++ Vaulter/UI/Navigation/TabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarController.swift
+//  Vaulter
+//
+//  Created by Roman on 07.01.2023.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+}
+
+extension TabBarController {
+    
+}
