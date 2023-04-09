fd42f7ead35f8b03314e6c0ce483128c0ae54d82
ungros <82379236+ungros@users.noreply.github.com>
2023-04-09 21:07:52 +0300

    fixes

--- Vaulter/UI/Controllers/GuardVC.swift
+++ Vaulter/UI/Controllers/GuardVC.swift
@@ -6,88 +6,15 @@
 //
 
 import UIKit
+import SnapKit
 
 final class GuardVC: RootViewController {
+  
+    let faceIDButton = FaceIDButton()
+    let 
     
-    private let faceIDButton = FaceIDButton()
-    private let passwordButton = PasswordButton(placeholder: R.Strings.GuardVC.placeholder)
-    private let eyeButton = EyeButton()
-    
-    private var isPrivate = true
-    
-    
-    
-    
-    
-    override func viewDidLoad() {
-        
-        super.viewDidLoad()
-        
-        view.backgroundColor = R.Colors.background
-        view.addSubview(faceIDButton)
-        view.addSubview(passwordButton)
-        
-        setupConstraints()
-        setupPasswordTextField()
-        
-        addActions()
-    }
-    
-    @objc
-    private func displayText() {
-        let imageName = isPrivate ? "eye" : "eye.splash"
-       
-        passwordButton.isSecureTextEntry.toggle()
-        eyeButton.setImage(UIImage(systemName: imageName), for: .normal)
-    
-        isPrivate.toggle()
-    }
 }
 
 private extension GuardVC {
     
-    func addActions() {
-        eyeButton.addTarget(self, action: #selector(displayText), for: .touchUpInside)
-    }
-    
-    func setupPasswordTextField() {
-        
-        passwordButton.delegate = self
-        passwordButton.rightView = eyeButton
-        passwordButton.rightViewMode = .always
-    }
-    
-    
-    func setupConstraints() {
-        
-        faceIDButton.translatesAutoresizingMaskIntoConstraints = false
-        passwordButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            
-            faceIDButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 250),
-            faceIDButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            faceIDButton.heightAnchor.constraint(equalToConstant: 50),
-            faceIDButton.widthAnchor.constraint(equalToConstant: 250),
-            
-            passwordButton.centerYAnchor.constraint(equalTo: faceIDButton.centerYAnchor, constant: 65),
-            passwordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            passwordButton.heightAnchor.constraint(equalToConstant: 50),
-            passwordButton.widthAnchor.constraint(equalToConstant: 250),
-            
-            
-            
-            
-        ])
-        
-    }
-}
-
-extension GuardVC: UITextFieldDelegate {
-    
-    func textFieldDidChangeSelection(_ textField: UITextField) {
-        guard let text = textField.text else {return}
-        eyeButton.isEnabled = !text.isEmpty
-        eyeButton.tintColor = R.Colors.cardColor
-    }
 }
